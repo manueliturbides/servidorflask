d6f8f31849a067055a29bd2ee6018ab082ccf38c
manueliturbides <manueliturbides@hotmail.com>
2024-03-13 17:22:53 -0400

    a

--- cancelarpagos.py
+++ cancelarpagos.py
@@ -91,7 +91,6 @@ def consultarpagosacancelar():
           data = mycursor.fetchall()
           
           
-          
           conectar.close() 
           print(data) 
     except Exception as e:
@@ -123,10 +122,19 @@ def cancelarpagopornumeroderecibo():
           
           conectar = conectUserDatabase(row['parent'])
           mycursor = conectar.cursor(dictionary=True)
-          sql = "delete from pagosres where norecibo = "+"'"+str(row['norecibo'])+"'"
+          sql = "select vpagint,vpagmora,vpagcap from pagosres where norecibo = "+"'"+str(row['norecibo'])+"'"
+          mycursor.execute(sql)
+          misdatos = mycursor.fetchall()
+
+          mycursor = conectar.cursor()
+          sql = " update prestamos set vpagint = "+"'"+str(misdatos[0]['vpagint'])+"', vpagmora = "+"'"+str(misdatos[0]['vpagmora'])+"'\
+          ,vpagcap = "+"'"+misdatos[0]['vpagcap']+"' status = if(solicitado > vpagint+vpagcap, 'A','C') where noprest = "+"'"+str(row['noprest'])+"'"
+          mycursor.execute(sql)
+
+          sql = "update pagosres set cuota = 0, mora = 0, vpagint = 0, vpagmora = 0, vpagcap = 0, descinte = 0 where norecibo = "+"'"+str(row['norecibo'])+"'"
           mycursor.execute(sql)
           
-          sql = "delete from pagos where norecibo = "+"'"+str(row['norecibo'])+"'"
+          sql = "update pagos set cuota = 0, mora  = 0, vpagint = 0, vpagmora = 0, vpagcap = 0, descinte = 0 from pagos where norecibo = "+"'"+str(row['norecibo'])+"'"
           mycursor.execute(sql)
           
           
